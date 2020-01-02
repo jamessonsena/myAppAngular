f9e3b0d2fb30698a1072d76f6cc9a74e5acebb51
Jamesson Sales de Sena <jsdesena@interaxa.com>
2020-01-02 14:22:36 -0300

    chamando o router no app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {RouterModule} from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.router';
 
 @NgModule({
   declarations: [
@@ -18,9 +19,12 @@ import { ContatoComponent } from './institucional/contato/contato.component';
     SobreComponent,
     ContatoComponent  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash:false})]
   ],
-  providers: [],
+  providers: [
+    {provide:APP_BASE_HREF, useValue:'/teste'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
