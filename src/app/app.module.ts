f37987be9a658ac36b7ca28c1e2cf6ad0d2f41b0
Jamesson Sales de Sena <jsdesena@interaxa.com>
2020-01-06 17:17:45 -0300

    trabalhando com moedas no angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {APP_BASE_HREF} from '@angular/common';
+import {APP_BASE_HREF, registerLocaleData} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
-
-
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
