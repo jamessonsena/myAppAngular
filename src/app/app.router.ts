be2a443123f9ec403441ed60022631b11d2b3a54
Jamesson Sales de Sena <jsdesena@interaxa.com>
2020-01-06 16:22:14 -0300

    trabalhando com rotas

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,15 +2,15 @@ import {Routes} from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
-
 import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
+
 export const rootRouterConfig:Routes =[
     {path:'', redirectTo: '/home',pathMatch:'full'},
     {path:'home', component: HomeComponent },
     {path:'contato', component: ContatoComponent },
     {path:'sobre', component: SobreComponent },    
-    {path:'pets', component: ListaProdutoComponent }
-
+    {path:'pets', component: ListaProdutoComponent },
+    {path:'pets-detalhe/:id', component: ListaProdutoComponent }
 
 
 ];
